73ab76a85ef2357ec7ec54577208ebb7be13955c
Gang Liao <gangliao@umd.edu>
2018-07-20 01:43:42 +0800

    update

--- build-client.sh
+++ build-client.sh
@@ -6,7 +6,6 @@ cd freeflow
 # Install Memory Pool
 cd libmempool && make && make install && cd ..
 
-
 cd libraries/libmlx4-1.2.1mlnx1
 ./autogen.sh
 ./configure --prefix=/usr/ --libdir=/usr/lib/ --sysconfdir=/etc/
