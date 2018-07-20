701be397a1e686091278f66f6e03336bbcaa99dd
Gang Liao <gangliao@umd.edu>
2018-07-20 09:32:54 +0800

    update

--- install_driver.sh
+++ install_driver.sh
@@ -5,5 +5,5 @@ set -xe
 wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.2-1.0.0.0/MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64.tgz
 gunzip < MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64.tgz | tar xvf -
 cd  MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64
-yes | ./mlnxofedinstall
+echo 'Y' | ./mlnxofedinstall
 cd .. && rm -rf MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64 MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64.tgz
