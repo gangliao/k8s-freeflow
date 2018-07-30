648d6d0e4a18bcacdc88d7fe158d07cf973134ea
Gang Liao <gangliao@umd.edu>
2018-07-30 22:20:54 +0800

    fix typo

--- freeflow/ffrouter/ffrouter.cpp
+++ freeflow/ffrouter/ffrouter.cpp
@@ -38,7 +38,7 @@ size_t process_data(void *buffer, size_t size, size_t nmemb, void *user_p)
     }
 
     std::string value = std::string(node["value"]);
-    strncpy(buser_p, value.c_str(), sizeof(user_p));
+    strncpy(user_p, value.c_str(), sizeof(user_p));
 
     return size * nmemb;
 }
