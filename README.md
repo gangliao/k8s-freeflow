56409ea290a99c5ba3e1aa7cec2981be90838ff0
Gang Liao <gangliao@umd.edu>
2018-07-30 14:22:24 +0800

    update driver version

--- README.md
+++ README.md
@@ -25,7 +25,8 @@ MAINTAINER Gang Liao <gangliao@cs.umd.edu>
 RUN yum install -y wget git curl sed grep vim make gcc-c++ libnl3-devel libtool && \
     yum clean all
 
-RUN yum install -y pciutils numactl-libs gtk2 atk cairo gcc-gfortran tcsh lsof ethtool tcl tk && \
+RUN yum install -y libxml2-python pciutils numactl-libs gtk2 atk cairo \
+    gcc-gfortran tcsh lsof ethtool tcl tk && \
     yum clean all
 
 # git credential to skip password typing
--- install_driver.sh
+++ install_driver.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -xe
 
+MAJOR_VER=$(lsb_release -r --short | cut -d. -f1)
+MINOR_VER=$(lsb_release -r --short | cut -d. -f2)
+VER=$MAJOR_VER.$MINOR_VER
+
 # Install MLNX Driver
-wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.2-1.0.0.0/MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64.tgz
-gunzip < MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64.tgz | tar xvf -
-cd  MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64
+wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.2-1.0.0.0/MLNX_OFED_LINUX-4.2-1.0.0.0-rhel$VER-x86_64.tgz
+gunzip < MLNX_OFED_LINUX-4.2-1.0.0.0-rhel$VER-x86_64.tgz | tar xvf -
+cd  MLNX_OFED_LINUX-4.2-1.0.0.0-rhel$VER-x86_64
 echo 'Y' | ./mlnxofedinstall
-cd .. && rm -rf MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64 MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64.tgz
+cd .. && rm -rf MLNX_OFED_LINUX-4.2-1.0.0.0-rhel$VER-x86_64 MLNX_OFED_LINUX-4.2-1.0.0.0-rhel$VER-x86_64.tgz
