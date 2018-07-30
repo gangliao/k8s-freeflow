e71e5b9c3d38295919702ab6492293db7178d48f
Gang Liao <gangliao@umd.edu>
2018-07-30 22:06:56 +0800

    add update host list

--- freeflow/ffrouter/ffrouter.cpp
+++ freeflow/ffrouter/ffrouter.cpp
@@ -2,11 +2,91 @@
 // Licensed under the MIT License.
 
 #include "ffrouter.h"
+
+#include <curl/curl.h>
+#include <glog/logging.h>
+#include <json/json.h>
 #include <ifaddrs.h>
+
 #include "rdma_api.h"
 #include "verbs_cmd.h"
 
-void update_host_list() {}
+static const char *pCertFile   = "/etc/etcd/ssl/etcd.pem";
+static const char *pCACertFile = "/etc/kubernetes/ssl/ca.pem";
+static const char *pKeyName    = "/etc/etcd/ssl/etcd-key.pem";
+static const char *pKeyType    = "PEM";
+static const char *pCurlUrl    = "https://10.142.104.73/v2/keys/";
+
+size_t process_data(void *buffer, size_t size, size_t nmemb, void *user_p)
+{
+    Value root;
+    Value node;
+    Reader reader;
+    FastWriter writer;
+    string json = (char *)buffer;
+
+    if (!reader.parse(json, root))
+    {
+        std::cout << "parse json error" << std::endl;
+        return 0;
+    }
+    string nodeString = writer.write(root["node"]);
+    if (!reader.parse(nodeString, node))
+    {
+        std::cout << "parse json error" << std::endl;
+        return 0;
+    }
+
+    strncpy(user_p, node["value"].c_str(), node["value"].length());
+
+    return size * nmemb;
+}
+
+void update_host_list() {
+    CURLcode return_code;
+    return_code = curl_global_init(CURL_GLOBAL_SSL);
+
+    CHECK_EQ(CURLE_OK, return_code) << "init libcurl failed.";
+
+    CURL *easy_handle = curl_easy_init();
+    CHECK_NOTNULL(easy_handle);
+
+    char buff_p[16] = NULL;
+
+    curl_easy_setopt(easy_handle, CURLOPT_URL, "https://10.142.104.73/v2/keys/Microsoft");
+    curl_easy_setopt(easy_handle, CURLOPT_PORT, 2379);
+
+    /* since PEM is default, we needn't set it for PEM */
+    curl_easy_setopt(easy_handle, CURLOPT_SSLCERTTYPE, "PEM");
+
+    /* set the cert for client authentication */
+    curl_easy_setopt(easy_handle, CURLOPT_SSLCERT, pCertFile);
+
+    /* set the private key (file or ID in engine) */
+    curl_easy_setopt(easy_handle, CURLOPT_SSLKEYTYPE, pKeyType);
+    curl_easy_setopt(easy_handle, CURLOPT_SSLKEY, pKeyName);
+
+    /* set the file with the certs vaildating the server */
+    curl_easy_setopt(easy_handle, CURLOPT_CAINFO, pCACertFile);
+
+    /* disconnect if we can't validate server's cert */
+    curl_easy_setopt(easy_handle, CURLOPT_SSL_VERIFYPEER, 1L);
+
+    curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, &process_data);
+    curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, buff_p);
+
+    std::cout << buff_p << std::endl; 
+
+    CURLcode res = curl_easy_perform(easy_handle);
+    /* Check for errors */
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+
+    curl_easy_cleanup(easy_handle);
+    curl_global_cleanup();    
+}
 
 void mem_flush(const void *p, int allocation_size)
 {
