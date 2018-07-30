c568b6654b31009095178c9901ea334a8cb4a3fc
Gang Liao <gangliao@umd.edu>
2018-07-30 13:51:21 +0800

    generate dockerfile

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,13 @@
-FROM centos:7.4.1708
+#!/bin/bash
+set -xe
+
+VERSION=$(lsb_release -r --short)
+OSID=$(lsb_release -i --short  | awk '{print tolower($0)}')
+
+echo "$OSID:$VERSION"
+
+cat > Dockerfile << EOF
+FROM OSID:$VERSION
 
 MAINTAINER Gang Liao <gangliao@cs.umd.edu>
 
@@ -10,3 +19,4 @@ RUN yum install -y pciutils numactl-libs gtk2 atk cairo gcc-gfortran tcsh lsof e
 
 # git credential to skip password typing
 RUN git config --global credential.helper store
+EOF
