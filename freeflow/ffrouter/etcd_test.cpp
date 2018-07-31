3d9914168d020ba2caa869c920815bf0eaed4450
Gang Liao <gangliao@umd.edu>
2018-07-31 14:28:21 +0800

    add watch test

--- freeflow/ffrouter/etcd_test.cpp
+++ freeflow/ffrouter/etcd_test.cpp
@@ -1,7 +1,9 @@
+#include <b64.h>
 #include <curl/curl.h>
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 #include <json/json.h>
+#include <stddef.h>
 
 size_t process_data(void *buffer, size_t size, size_t nmemb, void *user_p)
 {
@@ -61,6 +63,58 @@ TEST(ETCD, GetValue)
     /* set the file with the certs vaildating the server */
     curl_easy_setopt(easy_handle, CURLOPT_CAINFO, pCACertFile);
 
+    /* disconnect if we can't validate server's cert */
+    curl_easy_setopt(easy_handle, CURLOPT_SSL_VERIFYPEER, 1L);
+
+    curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, &process_data);
+    curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, &buff_p);
+
+    CURLcode res = curl_easy_perform(easy_handle);
+    /* Check for errors */
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+
+    std::cout << buff_p << std::endl;
+
+    curl_easy_cleanup(easy_handle);
+    curl_global_cleanup();
+}
+
+TEST(ETCD, WatchValueChange)
+{
+    static const char *pCACertFile = "/etc/kubernetes/ssl/ca.pem";
+
+    CURLcode return_code;
+    return_code = curl_global_init(CURL_GLOBAL_SSL);
+
+    EXPECT_EQ(CURLE_OK, return_code) << "init libcurl failed.";
+
+    CURL *easy_handle = curl_easy_init();
+    CHECK_NOTNULL(easy_handle);
+
+    std::string buff_p;
+
+    curl_easy_setopt(easy_handle, CURLOPT_URL, "https://10.142.104.73/v3alpha/kv/watch");
+    curl_easy_setopt(easy_handle, CURLOPT_PORT, 2379);
+
+    struct curl_slist *headers = NULL;
+    headers                    = curl_slist_append(headers, "Content-Type: application/json");
+    curl_easy_setopt(easy_handle, CURLOPT_HTTPHEADER, headers);
+
+    std::string key   = "Microsoft";
+    char *encoded_key = b64_encode((const unsigned char *)key.c_str(), key.length());
+
+    LOG(INFO) << "base64 encoding [Microsoft] to [" << encoded_key << "]";
+
+    char post_fields[1024];
+    sprintf(post_fields, "{\"create_request\": {\"key\": \"%s\"}}", encoded_key);
+    curl_easy_setopt(easy_handle, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(easy_handle, CURLOPT_POSTFIELDS, post_fields);
+
+    /* set the file with the certs vaildating the server */
+    curl_easy_setopt(easy_handle, CURLOPT_CAINFO, pCACertFile);
     /* disconnect if we can't validate server's cert */
     curl_easy_setopt(easy_handle, CURLOPT_SSL_VERIFYPEER, 1L);
 
