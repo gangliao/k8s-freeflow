968e420074e432a5aad8a7faa3afc4ab1b53d0a6
Gang Liao <gangliao@umd.edu>
2018-08-01 21:43:01 +0800

    update

--- ip_hunter/build_image.sh
+++ ip_hunter/build_image.sh
@@ -3,8 +3,7 @@
 set -xe
 
 export http_proxy=http://10.130.14.129:8080
-docker build --build-arg http_proxy=$http_proxy \
-             --build-arg https_proxy=$http_proxy \
-             --build-arg HTTP_PROXY=$http_proxy \
-             --build-arg HTTPS_PROXY=$http_proxy \
-             -t gangliao/golang:1.10 -f ./Dockerfile .
+
+docker run -it -d -b `pwd`:/go/ golang:1.10 --name gang_golang
+
+docker exec -it gang_golang "/go/run_ip2etcd.sh"
