d7be0f2221df4bc76328e9c0f6e10d70019c6cc2
Gang Liao <gangliao@umd.edu>
2018-08-01 21:54:07 +0800

    fix ip hunter run

--- ip_hunter/run.sh
+++ ip_hunter/run.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-docker run -it -d -v `pwd`:/go/ golang:1.10 --name gang_golang
+docker run --name gang_golang -it -d -v `pwd`:/go/ golang:1.10
 
 docker exec -it gang_golang "./run_ip2etcd.sh"
--- ip_hunter/run_ip2etcd.sh
+++ ip_hunter/run_ip2etcd.sh
@@ -3,6 +3,9 @@
 set -xe
 
 export http_proxy=http://10.130.14.129:8080
+export https_proxy=http://10.130.14.129:8080
+
+export GOBIN=$GOPATH/bin
 
 go get .
 
