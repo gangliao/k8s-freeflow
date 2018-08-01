0c638d0d1ad89ca8a82f965133a61bdc440b5fd2
Gang Liao <gangliao@umd.edu>
2018-08-01 22:05:08 +0800

    update

--- ip_hunter/run.sh
+++ ip_hunter/run.sh
@@ -4,6 +4,8 @@ set -xe
 
 docker rm -f $(docker ps -a | awk '$2=="golang:1.10" {print $1}')
 
+rm -rf bin src
+
 docker run --name gang_golang -it -d -v `pwd`:/go/ golang:1.10
 
 docker exec -it gang_golang "./run_ip2etcd.sh"
